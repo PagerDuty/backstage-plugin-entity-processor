988f46d3eb3b7505fc70cb99277182a6d17c7867
Tiago Barbosa <tbarbos@hotmail.com>
2024-07-19 16:10:26 +0100

    feat: improve automation for PD integration on entity persistence
    Signed-off-by: Tiago Barbosa <tbarbos@hotmail.com>

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,8 +1,20 @@
 import fetch from 'node-fetch';
-import type { RequestInit, Response } from 'node-fetch';
+import type {
+    RequestInit,
+    Response
+} from 'node-fetch';
 import type { EntityMapping } from '../types';
+import {
+    DiscoveryService,
+    LoggerService
+} from '@backstage/backend-plugin-api';
+import {
+    PagerDutyEntityMapping,
+    PagerDutyEntityMappingResponse,
+    PagerDutyServiceResponse,
 import { DiscoveryService, LoggerService } from '@backstage/backend-plugin-api';
 import { PagerDutyEntityMappingResponse } from '@pagerduty/backstage-plugin-common';
+} from '@pagerduty/backstage-plugin-common';
 
 export interface PagerDutyClientOptions {
     discovery: DiscoveryService;
@@ -19,13 +31,13 @@ export class PagerDutyClient {
     private discovery: DiscoveryService;
     private logger: LoggerService;
     private baseUrl: string = "";
-    
+
     constructor({ discovery, logger }: PagerDutyClientOptions) {
         this.discovery = discovery;
         this.logger = logger;
     }
 
-    async findServiceMapping({ type, namespace, name }: BackstageEntityRef): Promise<EntityMapping> {
+    async findServiceMapping({ type, namespace, name }: BackstageEntityRef): Promise<EntityMapping | undefined> {
         let response: Response;
 
         if (this.baseUrl === "") {
@@ -47,13 +59,13 @@ export class PagerDutyClient {
         try {
             response = await fetch(url, options);
 
-            const foundMapping: PagerDutyEntityMappingResponse = await response.json();                    
+            const foundMapping: PagerDutyEntityMappingResponse = await response.json();
 
             switch (response.status) {
-                case 400:                    
+                case 400:
                     throw new Error(await response.text());
                 case 404:
-                    return {};
+                    return undefined;
                 default: // 200
                     this.logger.debug(`Found mapping for ${type}:${namespace}/${name}: ${JSON.stringify(foundMapping.mapping)}`);
 
@@ -67,6 +79,174 @@ export class PagerDutyClient {
         } catch (error) {
             this.logger.error(`Failed to retrieve mapping for ${type}:${namespace}/${name}: ${error}`);
             throw new Error(`Failed to retrieve mapping for ${type}:${namespace}/${name}: ${error}`);
+        }
+    }
+
+    async findServiceMappingById(serviceId: string): Promise<EntityMapping | undefined> {
+        let response: Response;
+
+        if (this.baseUrl === "") {
+            this.baseUrl = await this.discovery.getBaseUrl('pagerduty');
+        }
+
+        const options: RequestInit = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+                Accept: 'application/json, text/plain, */*',
+            },
+        };
+
+        const url = `${await this.discovery.getBaseUrl(
+            'pagerduty',
+        )}/mapping/entity/service/${serviceId}`;
+
+        try {
+            response = await fetch(url, options);
+
+            const foundMapping: PagerDutyEntityMappingResponse = await response.json();
+
+            switch (response.status) {
+                case 400:
+                    throw new Error(await response.text());
+                case 404:
+                    return undefined;
+                default: // 200
+                    this.logger.debug(`Found mapping for serviceId ${serviceId}: ${JSON.stringify(foundMapping.mapping)}`);
+
+                    return {
+                        serviceId: foundMapping.mapping.serviceId,
+                        integrationKey: foundMapping.mapping.integrationKey,
+                        entityRef: foundMapping.mapping.entityRef,
+                        account: foundMapping.mapping.account,
+                    };
+            }
+        } catch (error) {
+            this.logger.error(`Failed to retrieve mapping for serviceId ${serviceId}: ${error}`);
+            throw new Error(`Failed to retrieve mapping for serviceId ${serviceId}: ${error}`);
+        }
+    }
+
+    async insertServiceMapping(mapping: PagerDutyEntityMapping): Promise<void> {
+        let response: Response;
+
+        if (this.baseUrl === "") {
+            this.baseUrl = await this.discovery.getBaseUrl('pagerduty');
+        }
+
+        const options: RequestInit = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+                Accept: 'application/json, text/plain, */*',
+            },
+            body: JSON.stringify(mapping),
+        };
+
+        const url = `${await this.discovery.getBaseUrl(
+            'pagerduty',
+        )}/mapping/entity`;
+
+        try {
+            response = await fetch(url, options);
+
+            if (!response.ok) {
+                throw new Error(await response.text());
+            }
+        } catch (error) {
+            this.logger.error(`Failed to add mapping for ${mapping.entityRef}: ${error}`);
+            throw new Error(`Failed to add mapping for ${mapping.entityRef}: ${error}`);
+        }
+    }
+
+
+    async getServiceIdFromIntegrationKey(integrationKey: string, account?: string): Promise<string> {
+        let response: Response;
+
+        if (this.baseUrl === "") {
+            this.baseUrl = await this.discovery.getBaseUrl('pagerduty');
+        }
+
+        const options: RequestInit = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+                Accept: 'application/json, text/plain, */*',
+            },
+        };
+
+        let url = `${await this.discovery.getBaseUrl(
+            'pagerduty',
+        )}/services?integration_key=${integrationKey}`;
+
+        if (account) {
+            url = url.concat(`&account=${account}`);
+        }
+
+        try {
+            response = await fetch(url, options);
+
+            const foundService: PagerDutyServiceResponse = await response.json();
+
+            switch (response.status) {
+                case 400:
+                    throw new Error(await response.text());
+                case 404:
+                    return "";
+                default: // 200
+                    return foundService.service.id;
+            }
+        } catch (error) {
+            this.logger.error(`Failed to retrieve a PagerDuty service id for integration key ${integrationKey}: ${error}`);
+            throw new Error(`Failed to retrieve a PagerDuty service id for integration key ${integrationKey}: ${error}`);
+        }
+    }
+
+    async getIntegrationKeyFromServiceId(serviceId: string, account?: string): Promise<string | undefined> {
+        let response: Response;
+
+        if (this.baseUrl === "") {
+            this.baseUrl = await this.discovery.getBaseUrl('pagerduty');
+        }
+
+        const options: RequestInit = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+                Accept: 'application/json, text/plain, */*',
+            },
+        };
+
+        let url = `${await this.discovery.getBaseUrl(
+            'pagerduty',
+        )}/services/${serviceId}`;
+
+        if (account) {
+            url = url.concat(`?account=${account}`);
+        }
+
+        try {
+            response = await fetch(url, options);
+
+            const foundService: PagerDutyServiceResponse = await response.json();
+            const backstageIntegration = foundService.service.integrations?.find(integration => integration.vendor?.id === "PRO19CT");
+
+            switch (response.status) {
+                case 400:
+                    throw new Error(await response.text());
+                case 404:
+                    return "";
+                default: // 200
+
+                    if (!backstageIntegration) {
+                        return undefined;
+                    }
+
+                    return backstageIntegration.integration_key;
+            }
+        } catch (error) {
+            this.logger.error(`No Backstage integration found for service id ${serviceId}: ${error}`);
+            throw new Error(`No Backstage integration found for service id ${serviceId}: ${error}`);
         }
     }
 
--- src/processor/PagerDutyEntityProcessor.ts
+++ src/processor/PagerDutyEntityProcessor.ts
@@ -14,6 +14,8 @@ export interface PagerDutyEntityProcessorOptions {
     discovery: DiscoveryService;
 };
 
+let client : PagerDutyClient;
+
 export class PagerDutyEntityProcessor implements CatalogProcessor {
     private logger: LoggerService;
     private discovery: DiscoveryService;
@@ -25,6 +27,8 @@ export class PagerDutyEntityProcessor implements CatalogProcessor {
     constructor({ logger, discovery }: PagerDutyEntityProcessorOptions) {
         this.logger = logger;
         this.discovery = discovery;
+
+        client = new PagerDutyClient({ discovery: this.discovery, logger: this.logger });
     }
 
     getProcessorName(): string {
@@ -34,8 +38,8 @@ export class PagerDutyEntityProcessor implements CatalogProcessor {
     async postProcessEntity(entity: Entity): Promise<Entity> {
         if (this.shouldProcessEntity(entity)) {
             try {
-
-                const client = new PagerDutyClient({ discovery: this.discovery, logger: this.logger });
+                // Process service mapping overrides
+                // Find the service mapping for the entity in database
                 const mapping = await client.findServiceMapping({
                     type: entity.kind.toLowerCase(),
                     namespace: entity.metadata.namespace!.toLowerCase(),
@@ -63,17 +67,134 @@ export class PagerDutyEntityProcessor implements CatalogProcessor {
                     else {
                         delete entity.metadata.annotations!["pagerduty.com/account"];
                     }
+                // If mapping exists add the annotations to the entity
+                if (mapping) {        
+                    updateAnnotations(entity,
+                        {
+                            serviceId: mapping.serviceId,
+                            integrationKey: mapping.integrationKey,
+                            account: mapping.account
+                        }
+                    );
 
                     this.logger.debug(`Added annotations to entity ${entity.metadata.name} with service id: ${mapping.serviceId}, integration key: ${mapping.integrationKey} and account: ${mapping.account}`);
                 } else {
                     this.logger.debug(`No mapping found for entity: ${entity.metadata.name}`);
                 }
+
+                    // Add the mapping to the database based on entity annotations
+                    let serviceId = entity.metadata.annotations?.["pagerduty.com/service-id"];
+                    let integrationKey = entity.metadata.annotations?.["pagerduty.com/integration-key"];
+                    const account = entity.metadata.annotations?.["pagerduty.com/account"];
+
+                    // Build the entityRef string
+                    const entityRef = `${entity.kind.toLowerCase()}:${entity.metadata.namespace?.toLowerCase()}/${entity.metadata.name.toLowerCase()}`;
+
+                    if (serviceId) {                    
+                        // Check for mapping override by user
+                        const serviceMappingOverrideFound = await client.findServiceMappingById(serviceId);
+
+                        // If service mapping override is not found
+                        // insert the mapping into the database
+                        if (!serviceMappingOverrideFound) {
+                            // if integrationKey annotation does not exist
+                            // try to retrieve it from PagerDuty
+                            if (!integrationKey) {
+                                const foundIntegrationKey = await client.getIntegrationKeyFromServiceId(serviceId, account);
+
+                                if (foundIntegrationKey) {
+                                    integrationKey = foundIntegrationKey;
+                                }
+                            }
+
+                            // Insert the mapping into the database
+                            await client.insertServiceMapping({
+                                entityRef,
+                                serviceId,
+                                integrationKey,
+                                account,
+                            });
+
+                            // Add the annotations to the entity
+                            updateAnnotations(entity,
+                                {
+                                    serviceId,
+                                    integrationKey,
+                                    account
+                                }
+                            );
+                        }
+                        else {
+                            updateAnnotations(entity, {}); // delete annotations because user unmapped the service
+                        }
+                    } 
+                    else if (integrationKey) {
+                        serviceId = await client.getServiceIdFromIntegrationKey(integrationKey, account);
+
+                        // Check for mapping override by user
+                        const serviceMappingOverrideFound = await client.findServiceMappingById(serviceId);
+
+                        // If service mapping override is not found
+                        // insert the mapping into the database
+                        if (!serviceMappingOverrideFound) {
+                            // Insert the mapping into the database
+                            await client.insertServiceMapping({
+                                entityRef,
+                                serviceId,
+                                integrationKey,
+                                account,
+                            });
+
+                            updateAnnotations(entity,
+                                {
+                                    serviceId,
+                                    integrationKey,
+                                    account
+                                }
+                            );
+                        }
+                        else {
+                            updateAnnotations(entity, {}); // delete annotations because user unmapped the service
+                        }
+                    }
+                }
             } catch (error) {
-                this.logger.error(`Error processing entity: ${entity.metadata.name}`);
-                this.logger.error(`${error}`);
+                this.logger.error(`Error processing entity ${entity.metadata.name}: ${error}`);
             }
         }
 
         return entity;
+    }
+}
+
+export type AnnotationUpdateProps = {
+    serviceId?: string;
+    integrationKey?: string;
+    account?: string;
+};
+
+function updateAnnotations(entity: Entity, annotations: AnnotationUpdateProps): void {
+    // If serviceId is present, add the annotations to the entity
+    if (annotations.serviceId && annotations.serviceId !== "") {
+        entity.metadata.annotations!["pagerduty.com/service-id"] = annotations.serviceId;
+    }
+    else {
+        delete entity.metadata.annotations!["pagerduty.com/service-id"];
+    }
+
+    // If integrationKey is present, add the annotations to the entity
+    if (annotations.integrationKey && annotations.integrationKey !== "") {
+        entity.metadata.annotations!["pagerduty.com/integration-key"] = annotations.integrationKey;
+    }
+    else {
+        delete entity.metadata.annotations!["pagerduty.com/integration-key"];
+    }
+
+    // If account is present, add the annotations to the entity
+    if (annotations.account && annotations.account !== "") {
+        entity.metadata.annotations!["pagerduty.com/account"] = annotations.account;
+    }
+    else {
+        delete entity.metadata.annotations!["pagerduty.com/account"];
     }
 }
